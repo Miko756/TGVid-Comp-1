688e7bdd60f8ef8a83ef0ea015e4d7422c95ae41
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-10-04 21:13:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "29759992")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "61f150cdca64b2916fa499d107393140") # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "20071888")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "1c4cb9d94b23282abd9ae2a87a521b53") # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8089138730:AAGjEcbGMS0futfhsofm1Un3raQf8Xr9-No") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002019186374') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
